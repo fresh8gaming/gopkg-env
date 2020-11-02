fe5dfcb0bb59d28ba298163261a1fa150c71edb1
Dom Udall <dom.udall@fresh8gaming.com>
2020-11-02 11:16:46 +0000

    Add code

---
+++ defaults.go
@@ -0,0 +1,42 @@
+package env
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+// GetenvString takes an envvar name, gets the value, and returns it if not blank.
+// If blank, it will return the provided default value.
+func GetenvString(k, defaultValue string) string {
+	value := os.Getenv(k)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+// GetenvInt takes an envvar name, gets the value, and returns it if not blank.
+// If blank, it will return the provided default value.
+func GetenvInt(k string, defaultValue int) int {
+	valueString := os.Getenv(k)
+	value, err := strconv.Atoi(valueString)
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
+
+// GetenvBool takes an envvar name, gets the value, and returns it if not blank.
+// If blank, it will return the provided default value.
+func GetenvBool(k string, defaultValue bool) bool {
+	value := strings.ToLower(os.Getenv(k))
+	switch value {
+	case "true", "1":
+		return true
+	case "false", "0":
+		return false
+	}
+
+	return defaultValue
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fresh8gaming/gopkg-env
+
+go 1.15
